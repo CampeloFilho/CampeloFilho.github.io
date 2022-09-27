c27bcc48f1d07aa46a9cc878f92291340b596334
CampeloFilho <90098981+CampeloFilho@users.noreply.github.com>
2022-09-26 23:33:34 -0300

    ajeitando o código de sim

--- teamomuppet/js/move.js
+++ teamomuppet/js/move.js
@@ -1,13 +1,13 @@
-$("#changer").click(function () {
-    $("changer").html("não");
+$("#change").click(function () {
+    $("#change").html("Sim");
     $("#move").show();
   });
   
   $("#move").hover(function () {
-    changer();
+    change();
   });
   
-  function changer() {
+  function change() {
     var i = Math.floor(Math.random() * 500) + 1;
     var j = Math.floor(Math.random() * 100) + 1;
     $("#move").css({ top: j + 'px', left: i + 'px' });
